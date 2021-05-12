285dbfd37e81e01bcb0d09fe0e4bd143cbf14ca6
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-05-12 22:38:00 +0200

    integrated Mongoose into my REST API

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// import mongoose
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // import packages
 const express = require('express'),
   bodyParser = require('body-parser'),
