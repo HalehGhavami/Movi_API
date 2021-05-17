2adbfe03f051f179e037508d4c9fd63e895e808e
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-05-17 11:42:57 +0200

    Update each URL endpoints and request methods

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// import mongoose
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-
-const Movies = Models.Movie;
-const Users = Models.User;
-
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 // import packages
 const express = require('express'),
   bodyParser = require('body-parser'),
@@ -17,37 +5,28 @@ const express = require('express'),
 
 // import morgan into my package
 const morgan = require('morgan');
-
 const app = express();
+// import mongoose
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 
-const movies = [
-  {
-    title: 'Amadeus',
-    description:
-      'The film follows a fictional rivalry between Mozart and Italian composer Antonio Salieri at the court of Emperor Joseph II',
-    director: 'Milos Forman',
-    genres: [' Biography', 'Drama', 'History', 'Music'],
+const Movies = Models.Movie;
+const Users = Models.User;
+// const Genres = Models.Genre;
+// const Directors = Models.Director;
 
-    image:
-      'https://www.amadeus-live.com/wp-content/uploads/2015/10/main-home-image5.jpg',
-  },
-  {
-    title: 'Rebecca',
-
-    description: 'The film is a gothic tale shot in black-and-white.',
-    director: 'Alfred Hitchcock',
-    genres: 'Romance',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Rebecca_%281939_poster%29.jpeg/1200px-Rebecca_%281939_poster%29.jpeg',
-  },
-];
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //  middleware
+app.use(bodyParser.json());
+
+//log requests to server
 app.use(morgan('common'));
 
 app.use(express.static('public'));
-
-app.use(bodyParser.json());
 
 // error-handling middleware function that will log all application-level errors to the terminal.
 app.use((err, req, res, next) => {
@@ -55,30 +34,69 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-//  GET
+// default text responce when at /
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
 
+//return JSON object when at /movies
 app.get('/movies', (req, res) => {
-  res.json(movies);
-});
-
-// Gets the data about a single movie, by title
-app.get('/movies/:title', (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.title === req.params.title;
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
     })
-  );
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.get('/movies/genres/:genres', (req, res) => {
-  res.send('Successful GET request returning a description of the genre');
+// GETS JSON movie info when looking for specific title
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.get('/movies/directors/:name', (req, res) => {
-  res.send('Successful GET request returning a description of the Director');
+//Return data about a genre (description) by name/title (e.g., "Drama")
+app.get('/movies/genres/:Genre', (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.Genre })
+    .then((genre) => {
+      res.status(201).json(genre.Genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+//get info on director when looking for specific director
+app.get('/movies/director/:Name', (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.Name })
+    .then((director) => {
+      res.status(201).json(director.Director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //Add a user
@@ -91,6 +109,7 @@ app.get('/movies/directors/:name', (req, res) => {
   Birthday: Date
 }*/
 app.post('/users', (req, res) => {
+  // check if a user with the username provided by the client already exists
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -103,6 +122,7 @@ app.post('/users', (req, res) => {
           Birthday: req.body.Birthday,
         })
           .then((user) => {
+            //this callback, send a response back to the client that contains both a status code and the document (called “user”) just created
             res.status(201).json(user);
           })
           .catch((error) => {
@@ -117,25 +137,122 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.put('/users/:username', (req, res) => {
-  res.send(
-    'The user: ' + req.params.username + ' ' + 'was successfully updated'
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate(
+    //First parameter
+    { Username: req.params.Username },
+    //Second parameter
+    {
+      //SET new values that extracted from the request body (meaning that they come from a request sent by the user).
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    //Third parameter
+    { new: true }, // This line makes sure that the updated document is returned
+    //Fourth parameter that itself have two parameters (any errors that might have occured + represents the document that was just updated )
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
   );
 });
 
-app.post('/users/:username/favourites/:title', (req, res) => {
-  res.send('Movie:' + req.params.title + ' ' + 'was added to favourites. ');
-});
-
-app.delete('/users/:username/favourites/:title', (req, res) => {
-  res.send(
-    'Movie:' + req.params.title + ' ' + 'has been removed from favourites'
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/favorites/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
   );
 });
 
-app.delete('/users/:username', (req, res) => {
-  res.send('User' + req.params.username + ' ' + 'was deleted.');
+// “Allow users to remove a movie from their list of favorites” endpoint
+app.delete('/users/:Username/favorites/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
+
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+/*
+   access documentation.html using express.static
+app.use('/documenation', express.static('public'));
+   error handling
+app.use(err, req, res, next) => {
+console.error(err.stack);
+res.status(500).send('Error');
+}  
+  listen on port
+app.listen(8080, () => console.log('Your app is listening on port 8080'));
+*/
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
